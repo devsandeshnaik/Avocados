2e8f1545763f85659eaa6ef61f108a0c1aabab0f
Sandesh Naik <devsandeshnaik@gmail.com>
2020-06-03 18:02:43 +0530

    Completed the Avocado Recipe App!!!!!!!

--- Avacados/View/RipeningStages.swift
+++ Avacados/View/RipeningStages.swift
@@ -9,8 +9,21 @@
 import SwiftUI
 
 struct RipeningStages: View {
+    var ripeningStages: [Ripening] = ripeningData
     var body: some View {
-        Text("Ripening Stages")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                HStack(alignment: .center, spacing: 25){
+                    ForEach(ripeningStages) {item in
+                        RipeningView(ripening: item)
+                    }
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                Spacer()
+            }
+        }
     }
 }
 
